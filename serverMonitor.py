553a5b40eb1d4d7f479338c799edfcbd3f2a9fe2
ultraxia <825764773@qq.com>
2018-01-05 23:22:34 +0800

    CPU message modify

--- serverMonitor.py
+++ serverMonitor.py
@@ -9,8 +9,8 @@ def monitorTask(bot):
         gl = bot.List('group', groupid)
         if gl is not None:
                 for group in gl:
-                        msg = '服务器 120.25.213.2 当前状态：'+'\n'
-                        cpu_msg = 'CPU负载：'+str(psutil.cpu_times().user)+'%'+'\n'
+                        msg = '服务器当前状态：'+'\n'
+                        cpu_msg = 'CPU负载：'+str((psutil.cpu_percent(0))+'%'+'\n'
                         mem_msg = '内存已使用：'+str(psutil.virtual_memory()[2])+'%'+'\n'
                         pids = psutil.pids()
                         for pid in pids:
