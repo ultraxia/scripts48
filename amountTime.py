333f2c4a69c529b64d1f160e1d0e775f879bb0c6
ultraxia <825764773@qq.com>
2018-02-08 16:31:20 +0800

    update

--- amountTime.py
+++ amountTime.py
@@ -1,3 +1,4 @@
+import random
 import requests
 import json
 import time
@@ -33,7 +34,7 @@ def project_name(pro_id):
     return project_name
 
 def getOrders(pro_id,startTime,endTime,backerStandard):
-    print('查询中.....'+'\n')
+    print('正在获取集资数据.....'+'\n')
     page = 1
     url = 'https://wds.modian.com/api/project/orders'
     while True:
@@ -51,7 +52,9 @@ def getOrders(pro_id,startTime,endTime,backerStandard):
             if startTime <= orderTime <= endTime:
                 money_list.append(float(data['backer_money']))
                 if float(data['backer_money']) >= float(backerStandard):
-                    name_list.append(data['nickname'])      
+                    name_list.append(data['nickname'])  
+            else:
+                break   
         if datas == []:
             break
 
@@ -66,24 +69,39 @@ def start(name_list):
     pro_id = input('请设置摩点项目ID：')
     start = input('请设置起始时间：')
     startTime = Time_format_conversion(year+start+sec)
-    end = input('请设置结束时间：(默认将以当前时间计算)')   
+    end = input('请设置结束时间：') 
+    ifrandom = input('是否抽奖(y/n):')
     if end == '':
         endTime = time.time()
     else:
         endTime = Time_format_conversion(year+end+sec)
-    backerStandard = input('请设置金额:(可跳过)')
+    backerStandard = input('请设置金额:')
     
-    if backerStandard == '':
-        backerStandard = 0
-        getOrders(pro_id,startTime,endTime,backerStandard)
-        print('设定时间段内累计达成集资%.2f元' % sum(money_list))
-    else:
+    if ifrandom == 'y':
         getOrders(pro_id,startTime,endTime,backerStandard)
         name_list = list(set(name_list))
-        print('在'+str(project_name(pro_id))+'中,'+str(start)+'后集资'+str(backerStandard)+'元以上共有'+str(len(name_list))+'人'+'\n')
-    
-        for ID in name_list:
-            print(ID+'\n')
+        print('设定时间段内集资%s元以上的聚聚分别有：\n\n' % backerStandard)
+        print(name_list)
+        print('\n即将开始抽奖\n')
+        count = input('请输入抽奖人数:')
+        print('\n抽奖中.....\n')
+        time.sleep(2)
+        winlist = random.sample(name_list, int(count))
+        for winner in winlist:
+            print('恭喜 %s 聚聚中奖！' % winner)
+            time.sleep(0.5)
+        print('\n')
+    else:
+        if backerStandard == '':
+            backerStandard = 0
+            getOrders(pro_id,startTime,endTime,backerStandard)
+            print('在'+str(project_name(pro_id))+'中，设定时间段内累计达成集资%.2f元' % sum(money_list))
+        else:
+            getOrders(pro_id,startTime,endTime,backerStandard)
+            name_list = list(set(name_list))
+            print('在'+str(project_name(pro_id))+'中，设定时间段内集资'+str(backerStandard)+'元以上共有'+str(len(name_list))+'人'+'\n')
+            print(name_list)
+            print('\n')
 
         
 
@@ -91,7 +109,6 @@ if __name__ == '__main__':
     print('Copyright (c) 2018 SNH48-费沁源应援会\nPowered by 奥特虾\n')
     time.sleep(1.5)
     start(name_list)
-    os.system('pause')
 
     
     
